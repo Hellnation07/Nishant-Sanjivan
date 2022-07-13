00077f00cf546a7ae5cc1aa1876e7241a752183b
Hellnation07 <bhartinishant26@gmail.com>
2022-07-13 18:10:16 +0530

    React Templates

--- src/App.js
+++ src/App.js
@@ -7,15 +7,31 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nishant Sanjivan <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/Hellnation07"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Projects
+        </a>
+        <a
+          className="App-link"
+          href="https://www.instagram.com/_hell_nation_0007/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Us
         </a>
       </header>
     </div>
